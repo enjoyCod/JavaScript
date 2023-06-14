fd81a259748bffd9519a059e84fc4dd15182afab
Kirill <102299849+enjoyCod@users.noreply.github.com>
2023-06-13 23:21:32 -0400

    Add files via upload
    Lesson_2

---
+++ HomeAssignments/Lesson-2/script.js
@@ -0,0 +1,63 @@
+ //let a = parseInt(prompt('First number', '0'))
+ //let b = parseInt(prompt('Second number', '0'))
+
+ //let s = a+b
+
+ //document.write(`<h1>Sum = ${s}</h1>`)
+
+//let userName = prompt('What is your name?, guest')
+//document.write(`Hello ${userName} on the website`)
+
+//count 2 boxes of popato
+
+//let boxeCount = parseInt(prompt('Count of box'))
+//let boxWeight = parseFloat(prompt('Count of Weight 1 boxes'))
+
+//let sum = boxeCount*boxWeight
+
+//document.write(`Total weight = ${sum.toFixed(2)}`)
+
+// 1. Input values 
+
+// let adultTicketPrice = parseFloat(prompt('Input coast Adult ticket price','0'))
+// let childTicketPrice = parseFloat(prompt('Input coast Child ticket price','0'))
+
+// let adultPassangersNumber = parseInt(prompt('Input count of adult passangers','0'))
+// let childPassangersNumber = parseInt(prompt('Input count of child passangers','0'))
+
+// // 2. Вычисления данных
+// let totalAdultTicketPrice = adultTicketPrice * adultPassangersNumber
+// let totalChildTicketPrice = childTicketPrice * childPassangersNumber
+
+// let totalPrice = totalAdultTicketPrice + totalChildTicketPrice
+
+// //3. Виведение результата
+
+// document.write(`Total amount = ${totalPrice}`)
+
+
+//  клавіатури; вводиться; орендна; плата; за; квартиру; за; 1; місяць.Визначити; скільки; треба; заплатити; за; 1; рік, за; 2; роки, за; 10; років
+
+//0
+// орендна плата за квартиру за 1 місяць - rentPerMonth
+//скільки треба платити за 1 рік -       rentPerYear
+//скільки треба платити за 1 роки -    rentPer2Years
+//скільки треба платити за 10 років - rentPer10Years
+
+//1. 	Вводимо необхідні змінні (rentPerMonth)
+
+let rentPerMonth = parseFloat(prompt('Input cost of rent per month','0'))
+
+// 2. Calculations
+
+let rentPerYear = rentPerMonth *12
+let rentPer2Years = rentPerMonth *24
+let rentPer10Years = rentPerMonth *120
+
+// 3. Output values
+
+document.write(`Per 1 year ${rentPerYear} </br>`)
+document.write(`Per 2 years ${rentPer2Years} </br>`)
+document.write(`Per 10 years ${rentPer10Years} </br>`)
+document.write(`Per 10 years ${rentPer10Years} </br>`)
+document.write(`Per 10 years ${rentPer10Years} </br>`)
